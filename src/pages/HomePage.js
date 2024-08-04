af6b65f4fa7d4a9f8c4ee7294a077dd0e1e5fa17
Ishika Agarwal <135102149+ishAg20@users.noreply.github.com>
2024-08-04 19:17:19 +0530

    Add files via upload

---
+++ src/pages/HomePage.js
@@ -0,0 +1,14 @@
+// HomePage.js
+import React from "react";
+import "./HomePage.css";
+
+const HomePage = () => {
+  return (
+    <div className="home-page">
+      <h1>Welcome to WellNest</h1>
+      <p>Your mental health and wellness companion.</p>
+    </div>
+  );
+};
+
+export default HomePage;
